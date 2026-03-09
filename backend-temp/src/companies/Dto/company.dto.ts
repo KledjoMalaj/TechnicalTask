0527ceb1ccbec4ff32cacaf4fdf3d8ba90dd5357
Kledjo Malaj <kledjomalaj20@gmail.com>
2026-03-09 20:43:02 +0100

    fix: remove ide and temp files

--- backend-temp/src/companies/Dto/company.dto.ts
+++
@@ -1,7 +0,0 @@
-import {IsNotEmpty, IsString} from "class-validator";
-
-export class CompanyDto {
-    @IsNotEmpty()
-    @IsString()
-    name:string
-}
--- backend-temp/src/schemas/Company.schema.ts
+++
@@ -1,9 +0,0 @@
-import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
-
-@Schema({timestamps:true})
-export class Company{
-    @Prop({unique:true,required:true})
-    name:string
-}
-
-export const CompanySchema = SchemaFactory.createForClass(Company)
